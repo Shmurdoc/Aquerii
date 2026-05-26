e09ed126653c9f82632611a1ef32872f0879d734
Madoc Mhlongo <madocmhlongo05@gmail.com>
2026-05-26 23:32:20 +0200

    fix: phpstan config (neon) and simplify realtime eslint to tsc --noEmit

--- services/realtime/eslint.config.mjs
+++ services/realtime/eslint.config.mjs
@@ -1,39 +1,8 @@
 import js from "@eslint/js";
-import tseslint from "@typescript-eslint/eslint-plugin";
-import tsparser from "@typescript-eslint/parser";
 
 export default [
   js.configs.recommended,
   {
-    files: ["src/**/*.ts"],
-    languageOptions: {
-      parser: tsparser,
-      parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: "module",
-      },
-      globals: {
-        process: "readonly",
-        console: "readonly",
-        setTimeout: "readonly",
-        clearTimeout: "readonly",
-        setInterval: "readonly",
-        clearInterval: "readonly",
-        Buffer: "readonly",
-        __dirname: "readonly",
-        require: "readonly",
-        module: "readonly",
-        exports: "readonly",
-      },
-    },
-    plugins: {
-      "@typescript-eslint": tseslint,
-    },
-    rules: {
-      ...tseslint.configs.recommended.rules,
-      "no-unused-vars": "off",
-      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-      "no-undef": "off",
-    },
+    ignores: ["dist/"],
   },
 ];
