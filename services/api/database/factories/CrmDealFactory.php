b5feb24e09fc4ded2fc432638c21b0c4470dd222
Aquerii Leader <madocmhlongo05@gmail.com>
2026-06-07 19:56:15 +0200

    fix(factory): CrmDealFactory sets workspace_id and pipeline_id

--- services/api/database/factories/CrmDealFactory.php
+++ services/api/database/factories/CrmDealFactory.php
@@ -2,7 +2,10 @@
 
 namespace Database\Factories;
 
+use App\Core\Models\Workspace;
 use App\Modules\CRM\Models\CrmDeal;
+use App\Modules\CRM\Models\CrmPipeline;
+use App\Modules\CRM\Models\CrmPipelineStage;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class CrmDealFactory extends Factory
@@ -12,6 +15,9 @@ class CrmDealFactory extends Factory
     public function definition(): array
     {
         return [
+            'workspace_id' => Workspace::factory(),
+            'pipeline_id' => CrmPipeline::factory(),
+            'stage_id' => CrmPipelineStage::factory(),
             'title' => fake()->sentence(3),
             'value' => fake()->randomFloat(2, 100, 100000),
             'currency' => 'USD',
